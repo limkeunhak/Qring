f719f62141b937df73b4e07f1855a86389ef2bcf
Keunhak Lim <limkeunhak@gmail.com>
2018-12-13 00:24:44 +0900

    Implement monitoring server page layout

--- monitoring/routes/index.js
+++ monitoring/routes/index.js
@@ -6,4 +6,20 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get("/question", function(req, res, next) {
+  res.status(200).json([{ question_id:"!!", user_id:"!@@", text:"#D", intent:"DD", date:"ddd", state:"@31"}]);
+});
+
+router.get("/answer", function(req, res, next) {
+  res.status(200).json();
+});
+
+router.get("/transaction", function(req, res, next) {
+  res.status(200).json();
+});
+
+router.get("/user", function(req, res, next) {
+  res.status(200).json();
+});
+
 module.exports = router;
