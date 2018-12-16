c7027245701f285826d1c3a8ca93c8b80c61e157
Keunhak Lim <limkeunhak@gmail.com>
2018-12-17 05:05:47 +0900

    Implements question saving

--- qna-store/routes/qnaRoute.js
+++ qna-store/routes/qnaRoute.js
@@ -1,5 +1,7 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+let mysqlDB = require('../utils/qring_db');
+mysqlDB.connect();
 
 router.get('/answer', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -22,7 +24,24 @@ router.get('/question', function(req, res, next) {
 });
 
 router.post('/question', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  let userKey = req.body.userKey;
+  let registDate = "0000-00-00";
+  let state = "ACTIVE";
+  let text = req.body.text;
+  let intent = "챗봇 인텐트"; // req.body.intent;
+  let entity = "테스트 엔티티";
+
+  let queryString = `insert into qring_question_tbl (user_key, text, intent, date, state) values ('${userKey}', '${text}', '${intent}', '${registDate}', '${state}')`;
+  console.log(queryString);
+
+  mysqlDB.query(queryString, function (err, rows, fields) {
+    if (err) {
+		console.log(err);
+      res.status(400).json(err);
+    } else {
+		res.status(200).json(rows);
+    }
+  })
 });
 
 router.delete('/question', function(req, res, next) {
