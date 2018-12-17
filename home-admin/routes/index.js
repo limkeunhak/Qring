d37cda8a22993821ac1f5146298a7664622e9240
Keunhak Lim <limkeunhak@gmail.com>
2018-12-18 01:20:33 +0900

    Implements each page layout

--- home-admin/routes/index.js
+++ home-admin/routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('home', { title: 'Express' });
+});
+
+router.get('/user', function(req, res, next) {
+  res.render('user');
+});
+
+router.get('/qna', function(req, res, next) {
+  res.render('qna');
 });
 
 module.exports = router;
