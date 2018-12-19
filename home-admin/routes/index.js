675f2bfefc0ad7301e3c4df74257c29d24279c55
Keunhak Lim <limkeunhak@gmail.com>
2018-12-19 22:46:22 +0900

    Implement adding question on home-admin qna table

--- home-admin/routes/index.js
+++ home-admin/routes/index.js
@@ -11,17 +11,18 @@ router.get('/user', function(req, res, next) {
   res.render('user');
 });
 
-router.get('/qna', function(req, res, next) {
-  res.render('qna');
-});
-
 router.get('/qna/questions', function(req, res, next) {
   axios.get('http://localhost:30030/qna/question')
   .then((result) => {
+	console.log(result.data);
     res.status(200).json(result.data);
   }).catch((ex) => {
     console.log(ex);
   });
+});
+
+router.get('/qna', function(req, res, next) {
+  res.render('qna');
 });
 
 module.exports = router;
